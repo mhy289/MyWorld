1e6dbb6dcfa46735e3e2f0f1d9460661459efe72
尘心_无愿 <2568237200@qq.com>
2025-09-26 21:25:54 +0800

    优化Element Plus图标导入方式，使用动态组件渲染

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 
 <script setup>
 import { ref, reactive, onMounted, computed } from 'vue';
-import { Cloud } from '@element-plus/icons-vue';
+import Cloud from '@element-plus/icons-vue/es/Cloud';
+import { ElIcon } from 'element-plus';
 
 // 多语言配置
 const translations = {
@@ -148,7 +149,7 @@ onMounted(() => {
       <!-- 装饰元素 -->
       <div class="flex justify-center mt-10">
         <el-icon :size="60" color="#3b82f6" :opacity="0.8">
-          <Cloud />
+          <component :is="Cloud" />
         </el-icon>
       </div>
     </div>
