a57f302bfb818d95791c063019d54d83a5de9901
尘心_无愿 <2568237200@qq.com>
2025-09-26 21:31:26 +0800

    优化Element Plus图标导入方式，使用解构赋值和动态组件

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 
 <script setup>
 import { ref, reactive, onMounted, computed } from 'vue';
-import { Cloud } from '@element-plus/icons-vue';
+import * as Icons from '@element-plus/icons-vue';
+const { Cloud } = Icons;
 import { ElIcon } from 'element-plus';
 
 // 多语言配置
@@ -149,7 +150,7 @@ onMounted(() => {
       <!-- 装饰元素 -->
       <div class="flex justify-center mt-10">
         <el-icon :size="60" color="#3b82f6" :opacity="0.8">
-          <Cloud />
+          <component :is="Cloud" />
         </el-icon>
       </div>
     </div>
